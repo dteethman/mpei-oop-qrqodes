4ea9448cf59a901b410378eed8524947bec788c8
dteethman <zuuuubice@gmail.com>
2021-04-27 20:41:34 +0300

    Initial Commit

---
+++ mpei-oop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mpei-oop
+//
+//  Created by Dmitry Zubkov on 27.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
