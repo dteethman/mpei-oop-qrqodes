4495dc769f3a8c36d1674abc95122204ecaa5fbf
dteethman <zuuuubice@gmail.com>
2021-05-10 13:47:35 +0300

    Add get info method

--- mpei-oop/Models/QRCode.swift
+++ mpei-oop/Models/QRCode.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class QRCode {
-    var stringValue: String!
+    var stringValue: String
     var title: String?
     var description: String?
     
@@ -11,8 +11,14 @@ class QRCode {
         self.description = nil
     }
     
+    init(stringValue: String, title: String, description: String?) {
+        self.stringValue = stringValue
+        self.title  = title
+        self.description = description
+    }
+    
     func getImage() -> UIImage? {
-        let data = self.stringValue.data(using: String.Encoding.ascii)
+        let data = self.stringValue.data(using: String.Encoding.utf8)
 
         if let filter = CIFilter(name: "CIQRCodeGenerator") {
             filter.setValue(data, forKey: "inputMessage")
@@ -24,7 +30,20 @@ class QRCode {
         }
 
         return nil
-
+    }
+    
+    func getInfo() -> NSAttributedString {
+        let resStr = NSMutableAttributedString()
+        let blackAtr = [NSMutableAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 17),
+                        NSMutableAttributedString.Key.foregroundColor: UIColor.label]
+        let plainAtr = [NSMutableAttributedString.Key.font: UIFont.systemFont(ofSize: 17),
+                        NSMutableAttributedString.Key.foregroundColor: UIColor.label]
+        let blackStr = NSMutableAttributedString(string:  "Text: ", attributes: blackAtr)
+        let plainStr = NSMutableAttributedString(string: stringValue, attributes: plainAtr)
+        
+        resStr.append(blackStr)
+        resStr.append(plainStr)
+        return resStr
     }
 }
 
